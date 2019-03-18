5c11f284616ab3956edb1b4c94b8c930a63e5341
Yovarni Yearwood <yovarniyearwood@gmail.com>
2019-03-18 01:51:27 -0400

    Sprite Construction
    Added sprite and tileset/tilemap pictures as well as C code and set up construction in code

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "assets\sprites.c"
+#include "gameCharacter.h"
+
+gameCharacter character;
+UBYTE spriteSize = 8;
+
+void movegamecharacter(gameCharacter* character, UINT8 x, UINT8 y){
+    move_sprite(character->spriteID[0], x, y);
+    move_sprite(character->spriteID[1], x + spriteSize, y);
+    move_sprite(character->spriteID[2], x, y + spriteSize);
+    move_sprite(character->spriteID[3], x + spriteSize, y + spriteSize);
+}
+
+void setupCharacter(){
+    /*  set_sprite_data(x, y, z)
+        Load sprite into memory
+        x - Starting tile index
+        y - Amount of tiles to load
+        z - Array to load tiles from
+    */
+    set_sprite_data(0, 4, characterSprite);
+
+    character.x = 24;
+    character.y = 24;
+    character.width = 16;
+    character.height = 16;
+
+    /*  set_sprite_tile(x, y)
+        Creates sprite in memory
+        x - Create the sprite at the given index
+        y - Set 'x' to the 'y' sprite in sprite data 
+    */
+    set_sprite_tile(0, 0);
+    set_sprite_tile(1, 2);
+    set_sprite_tile(2, 1);
+    set_sprite_tile(3, 3);
+    
+    // load tiles into character struct
+    character.spriteID[0] = 0;
+    character.spriteID[1] = 1;
+    character.spriteID[2] = 2;
+    character.spriteID[3] = 3;
+
+    movegamecharacter(&character, character.x, character.y);
+}
 
 void main(){
     initialization();
+    setupCharacter();   
+
+    /*  move_sprite(x, y, z)
+        Moves the sprite
+        x - sprite index
+        y - x-coordinate
+        z - y-coordinate
+    */
+
+    // Flag to show the sprites on-screen
+    SHOW_SPRITES;
 
     while(1){
         
---
+++ structures/gameCharacter.h
@@ -0,0 +1,7 @@
+typedef struct gameCharacter {
+    UBYTE spriteID[4];  
+    UINT8 x;            // Character's x position  
+    UINT8 y;            // Character's y position
+    UINT8 width;        // Character's width according to pixel size
+    UINT8 height;       // Character's height according to pixel size
+} gameCharacter;
