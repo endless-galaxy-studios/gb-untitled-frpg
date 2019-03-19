8bc8833165dddc38b2f8a1a83d9d6b9c9eca0535
Yovarni Yearwood <yovarniyearwood@gmail.com>
2019-03-19 14:23:12 -0400

    Scripts Initialization
    1. Created ggbgfx-sprites88-95.bat script to convert sprites 88-95 .png files to .c files
    2. Created README.md for further explanation about directory and scripts

---
+++ assets/scripts/ggbgfx-sprites88-95.bat
@@ -0,0 +1,70 @@
+@echo off
+
+echo Switching to sprites_png directory...
+echo.
+cd ..\graphics\sprites_png
+
+echo Copying sprites to temp directory...
+echo.
+xcopy sprite_088.png ..\sprites_c\temp /q
+xcopy sprite_089.png ..\sprites_c\temp /q
+xcopy sprite_090.png ..\sprites_c\temp /q
+xcopy sprite_091.png ..\sprites_c\temp /q
+xcopy sprite_092.png ..\sprites_c\temp /q
+xcopy sprite_093.png ..\sprites_c\temp /q
+xcopy sprite_094.png ..\sprites_c\temp /q
+xcopy sprite_095.png ..\sprites_c\temp /q
+
+echo Switching to temp directory...
+echo.
+cd ..\sprites_c\temp
+
+echo Converting sprites to .c files...
+echo.
+call ggbgfx sprite -o characterSprite_down1.c sprite_088.png
+call ggbgfx sprite -o characterSprite_down2.c sprite_089.png
+call ggbgfx sprite -o characterSprite_up1.c sprite_090.png
+call ggbgfx sprite -o characterSprite_up2.c sprite_091.png
+call ggbgfx sprite -o characterSprite_right1.c sprite_092.png
+call ggbgfx sprite -o characterSprite_right2.c sprite_093.png
+call ggbgfx sprite -o characterSprite_left1.c sprite_094.png
+call ggbgfx sprite -o characterSprite_left2.c sprite_095.png
+
+echo Copying .c files to sprites_c directory...
+echo.
+xcopy characterSprite_down1.c ..\ /q
+xcopy characterSprite_down2.c ..\ /q
+xcopy characterSprite_up1.c ..\ /q
+xcopy characterSprite_up2.c ..\ /q
+xcopy characterSprite_right1.c ..\ /q
+xcopy characterSprite_right2.c ..\ /q
+xcopy characterSprite_left1.c ..\ /q
+xcopy characterSprite_left2.c ..\ /q
+
+echo Deleting all temporary files...
+echo.
+del sprite_088.png
+del sprite_089.png
+del sprite_090.png
+del sprite_091.png
+del sprite_092.png
+del sprite_093.png
+del sprite_094.png
+del sprite_095.png
+del characterSprite_down1.c
+del characterSprite_down2.c
+del characterSprite_up1.c
+del characterSprite_up2.c
+del characterSprite_right1.c
+del characterSprite_right2.c
+del characterSprite_left1.c
+del characterSprite_left2.c
+
+echo Moving back to original directory...
+echo.
+cd ..\..\..\scripts
+
+echo All done!
+echo.
+pause
+cls
