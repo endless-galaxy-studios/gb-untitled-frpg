2afe7c3696f11bac7c3aafa1a3d5be91aa23fc2e
Yovarni Yearwood <yovarniyearwood@gmail.com>
2019-03-20 02:00:58 -0400

    Movement System Mechanics
    @TODO still have to finish this

--- src/main.c
+++ src/main.c
@@ -14,31 +14,71 @@ void performDelay(UINT8 numberOfLoops){
 }
 
 void main(){
+    UINT8 currentSpriteIndex = 1;
+    gameObject currentSprite;
+
     initialization();
-    setupCharacter();  
+    setupCharacter();
+
+    currentSprite.spriteID[0] = 0;
+    currentSprite.spriteID[1] = 1;
+    currentSprite.spriteID[2] = 2;
+    currentSprite.spriteID[3] = 3;
+
+    currentSprite.x = 24;
+    currentSprite.y = 24;
+    moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);  
 
     // Flag to show the sprites on-screen
     SHOW_SPRITES;
 
     while(1){
-        /*  @BUG I'm not sure why the below code works? Realistically, y + 2 
-            should make the sprite move UP, not DOWN, becuase math grid? LMAO.
-        */
-        if(joypad() & J_UP){
-            character_down1.y -= 2;
-            moveGameCharacter(&character_down1, character_down1.x, character_down1.y);
-        } else if(joypad() & J_DOWN){
-            character_down1.y += 2;
-            moveGameCharacter(&character_down1, character_down1.x, character_down1.y);
+        if(joypad() == J_UP){
+            if(currentSpriteIndex == 0){
+                currentSpriteIndex = 1;
+                
+                set_sprite_tile(0, 0);
+                set_sprite_tile(1, 2);
+                set_sprite_tile(2, 1);
+                set_sprite_tile(3, 3);
+            } else {
+                currentSpriteIndex = 0;
+
+                set_sprite_tile(0, 0);
+                set_sprite_tile(1, 2);
+                set_sprite_tile(2, 4);
+                set_sprite_tile(3, 5);
+            }
+
+            currentSprite.y -= 2;
+            moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);
+        } else if(joypad() == J_DOWN){
+            if(currentSpriteIndex == 0){
+                currentSpriteIndex = 1;
+                
+                set_sprite_tile(0, 6);
+                set_sprite_tile(1, 8);
+                set_sprite_tile(2, 7);
+                set_sprite_tile(3, 9);
+            } else {
+                currentSpriteIndex = 0;
+
+                set_sprite_tile(0, 6);
+                set_sprite_tile(1, 8);
+                set_sprite_tile(2, 10);
+                set_sprite_tile(3, 11);
+            }
+            currentSprite.y += 2;
+            moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);
         } 
         
-        if(joypad() & J_LEFT){
+        /*if(joypad() & J_LEFT){
             character_down1.x -= 2;
             moveGameCharacter(&character_down1, character_down1.x, character_down1.y);
         } else if(joypad() & J_RIGHT){
             character_down1.x += 2;
             moveGameCharacter(&character_down1, character_down1.x, character_down1.y);
-        } 
+        }*/
         performDelay(5);
     }
 }
