909f99d0878221d1c05209581f868c459b448b25
Yovarni Yearwood <yovarniyearwood@gmail.com>
2019-03-20 13:29:43 -0400

    Implemented moving character animation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <gb/gb.h>
 #include "src\setupSprite.c"
 
+UINT8 currentSpriteIndex = 1;
+gameObject currentSprite;
+
 void initialization(){
     DISPLAY_ON;		    // Turn on the display
 	NR52_REG = 0x8F;	// Turn on the sound
@@ -13,27 +16,9 @@ void performDelay(UINT8 numberOfLoops){
     for(i = 0; i < numberOfLoops; i++) wait_vbl_done();
 }
 
-void main(){
-    UINT8 currentSpriteIndex = 1;
-    gameObject currentSprite;
-
-    initialization();
-    setupCharacter();
-
-    currentSprite.spriteID[0] = 0;
-    currentSprite.spriteID[1] = 1;
-    currentSprite.spriteID[2] = 2;
-    currentSprite.spriteID[3] = 3;
-
-    currentSprite.x = 24;
-    currentSprite.y = 24;
-    moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);  
-
-    // Flag to show the sprites on-screen
-    SHOW_SPRITES;
-
-    while(1){
-        if(joypad() == J_UP){
+void checkInput(){
+    switch(joypad()){
+        case J_UP:
             if(currentSpriteIndex == 0){
                 currentSpriteIndex = 1;
                 
@@ -52,7 +37,9 @@ void main(){
 
             currentSprite.y -= 2;
             moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);
-        } else if(joypad() == J_DOWN){
+            break;
+
+        case J_DOWN:
             if(currentSpriteIndex == 0){
                 currentSpriteIndex = 1;
                 
@@ -68,17 +55,78 @@ void main(){
                 set_sprite_tile(2, 10);
                 set_sprite_tile(3, 11);
             }
+
             currentSprite.y += 2;
             moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);
-        } 
-        
-        /*if(joypad() & J_LEFT){
-            character_down1.x -= 2;
-            moveGameCharacter(&character_down1, character_down1.x, character_down1.y);
-        } else if(joypad() & J_RIGHT){
-            character_down1.x += 2;
-            moveGameCharacter(&character_down1, character_down1.x, character_down1.y);
-        }*/
-        performDelay(5);
+            break;
+
+        case J_LEFT:
+            if(currentSpriteIndex == 0){
+                currentSpriteIndex = 1;
+                
+                set_sprite_tile(0, 12);
+                set_sprite_tile(1, 14);
+                set_sprite_tile(2, 13);
+                set_sprite_tile(3, 15);
+            } else {
+                currentSpriteIndex = 0;
+
+                set_sprite_tile(0, 16);
+                set_sprite_tile(1, 18);
+                set_sprite_tile(2, 17);
+                set_sprite_tile(3, 19);
+            }
+
+            currentSprite.x -= 2;
+            moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);
+            break;
+
+        case J_RIGHT:
+            if(currentSpriteIndex == 0){
+                currentSpriteIndex = 1;
+                
+                set_sprite_tile(0, 20);
+                set_sprite_tile(1, 22);
+                set_sprite_tile(2, 21);
+                set_sprite_tile(3, 23);
+            } else {
+                currentSpriteIndex = 0;
+
+                set_sprite_tile(0, 24);
+                set_sprite_tile(1, 26);
+                set_sprite_tile(2, 25);
+                set_sprite_tile(3, 27);
+            }
+
+            currentSprite.x += 2;
+            moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);
+            break;
+    }
+    
+    performDelay(5);
+}
+
+void main(){
+
+    initialization();
+    setupCharacter();
+
+    currentSprite.spriteID[0] = 0;
+    currentSprite.spriteID[1] = 1;
+    currentSprite.spriteID[2] = 2;
+    currentSprite.spriteID[3] = 3;
+
+    currentSprite.x = 24;
+    currentSprite.y = 24;
+    moveGameCharacter(&currentSprite, currentSprite.x, currentSprite.y);  
+
+    // Flag to show the sprites on-screen
+    SHOW_SPRITES;
+
+    while(1){
+        // @TODO Offload these controls to controller.c
+        checkInput();
+
     }
 }
+
--- src/setupSprite.c
+++ src/setupSprite.c
@@ -14,10 +14,10 @@ void setupCharacter(){
     set_sprite_data(4, 2, characterSprite_up2);
     set_sprite_data(6, 4, characterSprite_down1);
     set_sprite_data(10, 2, characterSprite_down2);
-    /*set_sprite_data(16, 4, characterSprite_left1);
-    set_sprite_data(20, 4, characterSprite_left2);
-    set_sprite_data(24, 4, characterSprite_right1);
-    set_sprite_data(28, 4, characterSprite_right2);*/
+    set_sprite_data(12, 4, characterSprite_left1);
+    set_sprite_data(16, 4, characterSprite_left2);
+    set_sprite_data(20, 4, characterSprite_right1);
+    set_sprite_data(24, 4, characterSprite_right2);
 
     /*  set_sprite_tile(x, y)
         Creates sprite in memory
@@ -28,39 +28,4 @@ void setupCharacter(){
     set_sprite_tile(1, 2);
     set_sprite_tile(2, 1);
     set_sprite_tile(3, 3);
-    
-    /*set_sprite_tile(4, 4);
-    set_sprite_tile(5, 6);
-    set_sprite_tile(6, 5);
-    set_sprite_tile(7, 7);
-    
-    set_sprite_tile(8, 8);
-    set_sprite_tile(9, 10);
-    set_sprite_tile(10, 9);
-    set_sprite_tile(11, 11);
-    
-    set_sprite_tile(12, 12);
-    set_sprite_tile(13, 14);
-    set_sprite_tile(14, 13);
-    set_sprite_tile(15, 15);
-    
-    set_sprite_tile(16, 16);
-    set_sprite_tile(17, 18);
-    set_sprite_tile(18, 17);
-    set_sprite_tile(19, 19);
-    
-    set_sprite_tile(20, 20);
-    set_sprite_tile(21, 22);
-    set_sprite_tile(22, 21);
-    set_sprite_tile(23, 23);
-    
-    set_sprite_tile(24, 24);
-    set_sprite_tile(25, 26);
-    set_sprite_tile(26, 25);
-    set_sprite_tile(27, 27);
-    
-    set_sprite_tile(28, 28);
-    set_sprite_tile(29, 30);
-    set_sprite_tile(30, 29);
-    set_sprite_tile(31, 31);*/
 }
